af0b310cb21652ee55aa7d4aa4de2857d458c41c
code-with-santanu <santanupal003@gmail.com>
2023-10-07 18:53:09 +0530

    window eventListener is not working

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,63 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [status, setStatus] = useState("checking...");
   const [ip, setIp] = useState("");
   const [strength, setStrength] = useState("");
 
+  useEffect(() => {
+    //This code is executed in the browser
+  }, []);
+
+  // const g =
+  //   typeof globalThis === "object"
+  //     ? globalThis
+  //     : typeof window === "object"
+  //     ? window
+  //     : typeof global === "object"
+  //     ? global
+  //     : null; // Causes an error on the next line
+
+  // g.addEventListener("load", checkInternetconnection);
+
+  const checkInternetconnection = () => {
+    if (navigator.onLine) {
+      console.log("h1");
+      fetch("https://api.ipify.org?format=json")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("h2");
+          setIp(data.ip);
+          console.log("h3");
+          setStatus("Connected");
+
+          const connection = navigator.connection;
+          const networkStrength = connection
+            ? connection.downlink + "Mbps"
+            : "Unknown";
+          console.log("h4");
+          setStrength(networkStrength);
+        })
+        .catch(() => {
+          setStatus("Disconnected");
+          setIp("Unknown");
+          setStrength("Unknown");
+        });
+    } else {
+      console.log("err");
+      setStatus("Disconnected");
+      setIp("Unknown");
+      setStrength("Unknown");
+    }
+  };
+
+  if (process.browser) {
+    console.log("h5");
+    window.addEventListener("load", checkInternetconnection);
+  }
   return (
     <div className="container">
       <h1 className="heading">Internet Connection Status Checker</h1>
@@ -22,6 +72,7 @@ export default function Home() {
           Network Strength : <span>{strength}</span>
         </p>
       </div>
+      {/* <button onClick={checkInternetconnection}>click</button> */}
     </div>
   );
 }
